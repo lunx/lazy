cbc891f480073cb3e9cab8365946bef5a5e9bf0d
lunx <xl10230@126.com>
2013-01-28 17:00:26 +0800

    add jar,web project

---
+++ lazy-practice-web/src/main/java/com/lunx/lazy/practice/App.java
@@ -0,0 +1,16 @@
+package com.lunx.lazy.practice;
+/**
+ * @author lunx
+ * 
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
